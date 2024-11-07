e7939bc2d5453288ebcc20e81b81c65b72c962d1
CJ Molina6969 <mizuuchiha123456@gmail.com>
2024-11-07 08:08:03 +0800

    Add files via upload

---
+++ Testing.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	string name;
+	
+	cout << "Enter your name:  ";
+	getline(cin, name);
+	
+	cout << "Hello " << name << " You're my cutie patootie," << " always take care my homie";
+	
+	return 0;
+		
+
+	
+}

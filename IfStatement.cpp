aed211b712f04f239f10db8882339434b54a6dbe
CJ Molina6969 <mizuuchiha123456@gmail.com>
2024-11-07 08:08:56 +0800

    Add files via upload

---
+++ IfStatement.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+
+{
+	
+	char choice;
+	cout << "Is it raining? [Y/N]: ";
+	cin >> choice;
+	if(choice == 'Y') {
+		cout << "Get an umbrella";
+	}
+	else if(choice == 'N') {
+		cout << "It is sunny";
+	} else {
+		cout << "Invalid Input";
+	}
+
+	return 0;
+	
+}
